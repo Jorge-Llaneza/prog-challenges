1c1be6d92c6e09b2b81c6848662364f5efbeaf2a
jorge <llanezarodriguezjorge@gmail.com>
2025-10-13 13:59:35 +0200

    code working, but submission is bugged

---
+++ leetcode/108.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: list[int]) -> Optional[TreeNode]:
+            
+        def helper(l, r):
+            if l > r:
+                return None
+            m = (l + r) // 2
+            node = TreeNode(nums[m])
+            node.left = helper(l, m-1)
+            node. right = helper(m+1, r)
+            return node
+        return helper(0, len(nums) - 1)
+            
+        
