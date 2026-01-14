e4aef0d61b331ab3c06a478595c6e1980f3eee09
Jorge <llanezarodriguezjorge@gmail.com>
2026-01-14 21:04:06 +0100

    solved 238 efficiently

--- 238.py
+++ 238.py
@@ -1,30 +1,32 @@
 from typing import List
 import math
 class Solution:
-    nums = []
-    sol = []
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        self.nums = nums
-        self.sol = [0 for _ in nums]
+        
+        right = 1
+        sol = [1] * len(nums)
 
-        self.productExceptSelfN(0, len(nums) - 1, 1)
-        return self.sol
+        for n in range(len(nums)-1):
+            sol[n+1] = nums[n] * sol[n]
 
-    def productExceptSelfN(self, p1, p2, n):
-        if p2 == p1 + 1:
-            self.sol[p1] = n * self.nums[p2]
-            self.sol[p2] = n * self.nums[p1]
-        elif p1 == p2:
-            self.sol[p1] = n
-        else:
-            p_middle: int = int((p1 + p2) / 2)
+        for n in range(len(nums) - 1, 0, -1):
+            right *= nums[n]
+            sol[n-1] *= right
 
-            n1 = math.prod(self.nums[p_middle + 1: p2 + 1]) * n
-            self.productExceptSelfN(p1, p_middle, n1)
+        return sol
 
-            n2 = math.prod(self.nums[p1: p_middle+1]) * n
-            self.productExceptSelfN(p_middle + 1, p2, n2)
-        return
+
+        
+
+
+         
+
+
+
+
+
+
+
     
 
-print(Solution().productExceptSelf([1,2,3,4,5]))
+print(Solution().productExceptSelf([1,2,3,4]))
